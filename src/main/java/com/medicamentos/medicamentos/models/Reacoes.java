ce702b1193053293ce1a939936d1e572cfbd9415
gringo9994 <48113313+gringo9994@users.noreply.github.com>
2021-01-21 11:59:20 -0300

    Add files via upload

---
+++ src/main/java/com/medicamentos/medicamentos/models/Reacoes.java
@@ -0,0 +1,125 @@
+
+
+
+package com.medicamentos.medicamentos.models;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.ForeignKey;
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Entity
+public class Reacoes {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@JsonInclude(Include.NON_NULL)
+	
+	private Long id;
+	@JsonInclude(Include.NON_NULL)
+	 
+	private String descricao;
+	 
+	
+	
+	//muitas reacoes para um medicamento
+	
+	
+	
+  //  @ManyToOne(cascade = CascadeType.ALL)
+    
+   
+ //   @JsonInclude(Include.NON_NULL)
+    
+	// private Medicamento medicamento;
+	 
+
+	
+	
+	
+	public Reacoes() {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+	public Reacoes(Long id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+
+
+
+
+
+
+
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+
+}
